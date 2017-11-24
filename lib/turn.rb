feb4149a08a803d5d81896b7e9ad10b2e17e1d16
James Ferguson <ncjamesferguson@gmail.com>
2017-11-24 16:13:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,7 +29,7 @@ end
  end
 
  def turn(board)
-   puts "Please enter 1-9"
+   puts "Please enter 1-9:"
    puts "Where would you like to go?"
    input = gets.strip
    index = input_to_index(input)
