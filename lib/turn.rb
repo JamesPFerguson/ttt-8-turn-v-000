61288ccfe0ea4377079e2bb1db84b8b5a8724177
James Ferguson <ncjamesferguson@gmail.com>
2017-11-24 15:46:58 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,18 @@
+def turn()
+end
+
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "-----------"
+ puts " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts "-----------"
+ puts " #{board[6]} | #{board[7]} | #{board[8]} " 
+ end
+ def input_to_index(mark)
+   mark.to_i-1
+ end
+
+ def move(board,index, character = "X")
+   character.upcase
+   board[index] = "#{character}"
+ end
